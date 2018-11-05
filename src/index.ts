177347c9259eac2042af2ec36ef404997047fa36
Chris Lucas <chris@hhd.com.au>
2018-11-05 14:00:57 +1100

    feat: Added basic rot display

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
     which is the webpack entry file
 */
 import "./assets/stylesheets/styles.scss"
+import { Display } from "rot-js"
 
 // React imports
 // import React from 'react';
@@ -11,3 +12,9 @@ import "./assets/stylesheets/styles.scss"
 
 // React Render to page
 // ReactDOM.render(<App />, document.getElementById('root'));
+let display = new Display({ width: 80, height: 50, layout: "rect" })
+let container = display.getContainer()
+document.body.appendChild(container)
+display.draw(5, 4, "@")
+display.draw(15, 4, "%", "#0f0") // foreground color
+display.draw(25, 4, "#", "#f00", "#009")
